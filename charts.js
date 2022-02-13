456294a43f8a5641bd374ce47a57f9ca87eec92a
AMH-4 <90002900+AMH-4@users.noreply.github.com>
2022-02-13 15:13:43 -0600

    Add files via upload

--- charts.js
+++ charts.js
@@ -106,7 +106,6 @@ function buildCharts(sample) {
   });
 }
 
-
 //Delvierable 2 - Bubble Chart 
 
  // 1. Create the trace for the bubble chart.
@@ -142,88 +141,3 @@ Plotly.newPlot('bubble', bubbleData, bubbleLayout, config);
 
 
 
-//Deliverable 3 - Gauge Chart
-
-    // Create a variable that filters the samples for the object with the desired sample number.
-    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
-
-    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
-    var metadataArray = data.metadata.filter(sampleObj => sampleObj.id == sample);
-
-    // Create a variable that holds the first sample in the array.
-    var result = resultArray[0];
-    console.log(result);
-  
-
-    // 2. Create a variable that holds the first sample in the metadata array.
-    var mresult = metadataArray[0];
-    console.log(mresult);
-    
-
-    // Create variables that hold the otu_ids, otu_labels, and sample_values.
-    var otu_ids = result.otu_ids;
-    var otu_labels = result.otu_labels;
-    var sample_values = (result.sample_values.slice(0,10)).reverse();
-    console.log(otu_ids)
-
-
-    // 3. Create a variable that holds the washing frequency.
-    var wfreq = mresult.wfreq;
-    console.log(wfreq);
-   
-    // Create the yticks for the bar chart.
-    var top_otu_ids = (data.samples[0].otu_ids.slice(0,10)).reverse();
-    var otu_id = top_otu_ids.map(d => "OTU " + d);
-    console.log(`OTU IDS: ${otu_id}`)
-
-    var yticks = otu_id;
-
-    // 4a. Save the element that was changed as a variable.
-
-    let changedElement = d3.select(this);
-
-    // 4b. Save the value that was changed as a variable.
-    let elementValue = changedElement.property("value")
-    console.log(elementValue);
-
-    var titleText = "<b>Belly Button Washing Frequency</b><br> Scrubs Per Week</br>";
-
-    // 4c. Create the trace for the gauge chart.
-    var trace = {
-      type: "indicator",
-      mode: "gauge+number",
-      value: wfreq,
-      gauge: { axis: { 
-        visible: true, 
-        range: [0, 10] },
-        bar: { color: "black"},
-        steps:[
-          { range: [0, 2], color: "red" },
-          { range: [2, 4], color: "darkorange" },
-          { range: [4, 6], color: "yellow" },
-          { range: [6, 8], color: "lightgreen" },
-          { range: [8, 10], color: "green" }
-         ] },
-		  title: { text: titleText }
-      //domain: { x: [0, 1], y: [0, 1] }
-      
-    };
-    var gaugeData = [trace];
-    
-    // 5. Create the layout for the gauge chart.
-    var gaugeLayout = {
-      autosize: true,
-      //width: 1900, height: 450 
-      //automargin: false
-      //paper_bgcolor: "lightblue",
-      margin: { t: 30, r: 30, l: 30, b: 30 }
-    };
-
-    var config = {responsive: true};
-
-    // 6. Use Plotly to plot the gauge data and layout.
-    Plotly.newPlot("gauge", gaugeData, gaugeLayout, config);
-  });
-};
-  
-  
